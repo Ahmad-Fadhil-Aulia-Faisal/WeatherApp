db8081f3485b216ad47aca7e5c5e513c5ae2fce4
AhmadFadhilAuliaFaisal <ahmadfadhilauliafaisal@gmail.com>
2024-06-23 21:51:48 +0700

    SETUP API
    Fitur pencarian sudah berhasil menampilkan data musim

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,29 +1,44 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { BASE_URL, API_KEY } from '../../src/components/constants'; // Pastikan Anda menyimpan konstanta dalam file constants.js
-import { View, StyleSheet } from 'react-native';
+import { BASE_URL, API_KEY } from '../../src/components/constants';
+import { View, StyleSheet, ActivityIndicator, Text } from 'react-native';
 import WeatherSearch from '../../src/components/weatherSearch';
 import WeatherInfo from '../../src/components/weatherInfo';
 
 const App = () => {
-  const [weatherData, setWeatherData] = useState(null); // State untuk menyimpan data cuaca
+  const [weatherData, setWeatherData] = useState(null);
+  const [status, setStatus] = useState('');
+  const [error, setError] = useState('');
 
   const searchWeather = (location: any) => {
+    setStatus('loading'); // Mengatur status ke "loading"
     axios
-      .get(`${BASE_URL}?q=${location}&appid=${API_KEY}&units=metric`) // tambahkan units=metric untuk satuan Celcius
+      .get(`${BASE_URL}?q=${location}&appid=${API_KEY}`)
       .then((response) => {
         const data = response.data;
-        setWeatherData(data); // Simpan data cuaca ke dalam state
+        data.visibility /= 1000;
+        data.visibility = data.visibility.toFixed(2);
+        data.main.temp -= 273.15; // Konversi Kelvin ke Celcius
+        data.main.temp = data.main.temp.toFixed(2);
+        setWeatherData(data);
+        setStatus('success'); // Mengatur status ke "success"
       })
       .catch((error) => {
         console.log(error);
+        setError('Something went wrong. Please try again with a correct city name.');
+        setStatus('error'); // Mengatur status ke "error"
       });
   };
 
   return (
     <View style={styles.container}>
       <WeatherSearch searchWeather={searchWeather} />
-      <WeatherInfo weatherData={weatherData} />
+      <View style={styles.marginTop20}>
+        {/* Menampilkan komponen berdasarkan status */}
+        {status === 'loading' && <ActivityIndicator size="large" />}
+        {status === 'success' && <WeatherInfo weatherData={weatherData} />}
+        {status === 'error' && <Text>{error}</Text>}
+      </View>
     </View>
   );
 };
@@ -31,6 +46,9 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     padding: 20,
+  },
+  marginTop20: {
+    marginTop: 20,
   },
 });
 
--- src/components/constants.js
+++ src/components/constants.js
@@ -1,2 +1,2 @@
 export const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
-export const API_KEY = 'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}';
+export const API_KEY = 'b1e7880cd609b7c2adbf17a596e6b1a5';
